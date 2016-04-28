f752db238070d51f5bf4bc97e3ce42cf851ed3f5
cascadianmetapod <dudeman@live.ca>
2016-04-28 01:32:52 -0700

    .6

--- ui/mods/cascadian/maplist.js
+++ ui/mods/cascadian/maplist.js
@@ -1,5 +1,6 @@
 ﻿cShareSystems.load_pas("An Uber map remix collection", [
 	"coui://ui/mods/cascadian/systems/amplusplus.pas",
+	"coui://ui/mods/cascadian/systems/xap.pas.pas",
 	"coui://ui/mods/cascadian/systems/bedlam_boss.pas",
 	"coui://ui/mods/cascadian/systems/clutch_x.pas",
 	"coui://ui/mods/cascadian/systems/crag_x.pas",
@@ -8,6 +9,6 @@
 	"coui://ui/mods/cascadian/systems/lock_x.pas",
 	"coui://ui/mods/cascadian/systems/pacific_x.pas",
 	"coui://ui/mods/cascadian/systems/rok.pas",
-	"coui://ui/mods/cascadian/systems/xap_system.pas.pas",
+	"coui://ui/mods/cascadian/systems/xap.pas.pas",
 	"coui://ui/mods/cascadian/systems/chokepoint.pas.pas"
 ]);
