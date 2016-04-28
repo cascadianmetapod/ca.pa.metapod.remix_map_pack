130d82f8ae56444d044ece5da93f238512114ca9
cascadianmetapod <dudeman@live.ca>
2016-04-28 01:08:44 -0700

    fixed directory and version

--- ui/mods/cascadian/maplist.js
+++ ui/mods/cascadian/maplist.js
@@ -1,13 +1,13 @@
 ﻿cShareSystems.load_pas("Remixed Systems", [
-	"coui://ui/mods/metapod/systems/forge_x.pas",
-	"coui://ui/mods/metapod/systems/amplusplus.pas",
-	"coui://ui/mods/metapod/systems/bedlam_boss.pas",
-	"coui://ui/mods/metapod/systems/clutch_x.pas",
-	"coui://ui/mods/metapod/systems/crag_x.pas",
-	"coui://ui/mods/metapod/systems/duat_x.pas",
-	"coui://ui/mods/metapod/systems/forge_x.pas",
-	"coui://ui/mods/metapod/systems/lock_x.pas",
-	"coui://ui/mods/metapod/systems/pacific_x.pas",
-	"coui://ui/mods/metapod/systems/rok.pas",
-	"coui://ui/mods/metapod/systems/xap_system.pas.pas"
+	"coui://ui/mods/cascadian/systems/forge_x.pas",
+	"coui://ui/mods/cascadian/systems/amplusplus.pas",
+	"coui://ui/mods/cascadian/systems/bedlam_boss.pas",
+	"coui://ui/mods/cascadian/systems/clutch_x.pas",
+	"coui://ui/mods/cascadian/systems/crag_x.pas",
+	"coui://ui/mods/cascadian/systems/duat_x.pas",
+	"coui://ui/mods/cascadian/systems/forge_x.pas",
+	"coui://ui/mods/cascadian/systems/lock_x.pas",
+	"coui://ui/mods/cascadian/systems/pacific_x.pas",
+	"coui://ui/mods/cascadian/systems/rok.pas",
+	"coui://ui/mods/cascadian/systems/xap_system.pas.pas"
 ]);
