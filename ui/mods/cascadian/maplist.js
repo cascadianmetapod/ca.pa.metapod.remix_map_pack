e9304c57817f87ad25dde88084b1e2508c4e2e33
cascadianmetapod <dudeman@live.ca>
2016-04-29 18:30:24 -0700

    redid xap, removed crag x

--- ui/mods/cascadian/maplist.js
+++ ui/mods/cascadian/maplist.js
@@ -3,11 +3,11 @@
 	"coui://ui/mods/cascadian/systems/xap.pas.pas",
 	"coui://ui/mods/cascadian/systems/bedlam_boss.pas",
 	"coui://ui/mods/cascadian/systems/clutch_x.pas",
-	"coui://ui/mods/cascadian/systems/crag_x.pas",
 	"coui://ui/mods/cascadian/systems/duat_x.pas",
 	"coui://ui/mods/cascadian/systems/forge_x.pas",
 	"coui://ui/mods/cascadian/systems/lock_x.pas",
 	"coui://ui/mods/cascadian/systems/pacific_x.pas",
 	"coui://ui/mods/cascadian/systems/rok.pas",
+	"coui://ui/mods/cascadian/systems/xap.pas",
     "coui://ui/mods/cascadian/systems/chokepoint.pas.pas"
 ]);
