e9b40e330e721c9ad466ac1db4ab777ca11bf6ce
cascadianmetapod <dudeman@live.ca>
2017-08-06 14:52:27 -0700

    Update maplist.js

--- ui/mods/cascadian/maplist.js
+++ ui/mods/cascadian/maplist.js
@@ -1,6 +1,6 @@
 ﻿cShareSystems.load_pas("An Uber map remix collection", [
 	"coui://ui/mods/cascadian/systems/amplusplus.pas",
-	"coui://ui/mods/cascadian/systems/xap.pas.pas",
+	
 	"coui://ui/mods/cascadian/systems/bedlam_boss.pas",
 	"coui://ui/mods/cascadian/systems/clutch_x.pas",
 	"coui://ui/mods/cascadian/systems/duat_x.pas",
@@ -8,7 +8,7 @@
 	"coui://ui/mods/cascadian/systems/lock_x.pas",
 	"coui://ui/mods/cascadian/systems/pacific_x.pas",
 	"coui://ui/mods/cascadian/systems/rok.pas",
-	"coui://ui/mods/cascadian/systems/xap.pas",
+	
 	"coui://ui/mods/cascadian/systems/roc_with_trees.pas",
 	"coui://ui/mods/cascadian/systems/lavamplus.pas",
 	"coui://ui/mods/cascadian/systems/lockery.pas",
